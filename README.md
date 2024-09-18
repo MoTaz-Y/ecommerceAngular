8de173301ab9c4a91724790fb6ac0360c272f498
ShadyAbdallah <ShadyAbdallahG98@gmail.com>
2024-09-18 20:38:16 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# MyAngularProject
+# Ecommerce
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.2.3.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { NavBarComponent } from './navbar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductsComponent, NavBarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'my-angular-project';
+  title = 'ecommerce';
 }
